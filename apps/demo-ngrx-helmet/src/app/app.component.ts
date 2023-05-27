b4efc25392aa7e74ed8043a447809453828b0a50
Chris <chris@deep-blue.io>
2023-05-27 12:43:57 +0200

    feat: add responsive nav-side-bar

--- apps/demo-ngrx-helmet/src/app/app.component.ts
+++ apps/demo-ngrx-helmet/src/app/app.component.ts
@@ -1,14 +1,27 @@
-import { Component } from "@angular/core";
-import { RouterModule } from "@angular/router";
-import { NxWelcomeComponent } from "./nx-welcome.component";
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { LayoutModule } from '@angular/cdk/layout';
 
 @Component({
+  selector: 'ngrx-helmet-root',
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
-  selector: "ngrx-helmet-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"],
+  imports: [
+    LayoutModule,
+    RouterModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule
+  ],
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss']
 })
 export class AppComponent {
-  title = "demo-ngrx-helmet";
+  title = 'demo-ngrx-helmet';
 }
